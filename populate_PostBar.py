4fac5c4bf77273f823fc331b36724879f6556e3e
zhouyang shen <2359009s@student.gla.ac.uk>
2019-02-28 13:30:49 +0000

    add populate question

--- populate_PostBar.py
+++ populate_PostBar.py
@@ -27,51 +27,59 @@ def populate():
 
     math_questions = [{"question_title": "What is the differentiation?",
                        "question_content": "I heard a lot about differentiation during hight school,what is that exactly",
-                       "username": user.username, "answer_id": 0, "views": 31, "likes": 12, "question_isComplete": True,
+                       "username": user.username, "id": 0, "views": 31, "likes": 12, "question_isComplete": True,
                        "latest_question_published": "2009-11-13"}]
 
     computing_science_questions = [
         {"question_title": "How am i supposed to learn Cs?", "question_content": "cause it is so difficult for me!",
-         "username": user.username, "answer_id": 2, "views": 31, "likes": 12, "question_isComplete": True,
+         "username": user.username, "id": 2, "views": 31, "likes": 12, "question_isComplete": True,
          "latest_question_published": "2009-11-13"}]
 
     other_questions = [{"question_title": "Do we have to attend any of the lectures?",
                         "question_content": "i know is not correct to skip any lectures,however i think some of them is not that kind of important",
-                        "username": user.username, "answer_id": 4, "views": 31, "likes": 12,
+                        "username": user.username, "id": 4, "views": 31, "likes": 12,
                         "question_isComplete": True, "latest_question_published": "2009-11-13"}]
 
     Cats = {"Math": {"question": math_questions}, "Computing_Science": {"question": computing_science_questions},
             "Other_Questions": {"question": other_questions}}
 
-    userprofile = {"popular": 120, "uesr": User.username}
+    userprofile = {"popular": 120, "user": user}
 
-    for c in Cats:
+    add_profile(userprofile["popular"], userprofile["user"])
+    for c, qs in Cats.items():
         add_cat(c)
+        for i, q in enumerate(qs["question"]):
+            add_question(c, user, q["question_title"], q["question_content"], q["views"], q["likes"],
+                         q["latest_question_published"], q["question_isComplete"])
+    for ad in answers:
+
 
 
 def add_cat(name):
-    c = Category.objects.get_or_create(name=name)
+    c = Category.objects.get_or_create(name=name)[0]
     c.save()
     return c
 
 
-def add_question(category, question_title, question_content, username, views, likes, question_isComplete,
-                 latest_question_published):
-    q = Question.objects.get_or_create(category=category, question_title=question_title,
-                                       question_content=question_content, username=username, views=views,
-                                       likes=likes, question_isComplete=question_isComplete,
-                                       latest_question_published=latest_question_published)
-    q.username = username
-    q.views = views
-    q.likes = likes
-    q.question_isComplete = question_isComplete
-    q.latest_question_published = latest_question_published
+def add_question(category, user, title, content, views, likes, latest_question_published, completed):
+    q = Question.objects.get_or_create(title=title,
+                                       content=content,
+                                       views=views,
+                                       likes=likes,
+                                       user=user,
+                                       category=category,
+                                       latest_question_published=latest_question_published,
+                                       completed=completed)[0]
     q.save()
     return q
 
 
-def add_answer(user: User, question: Question, question_title, content, rank_count=0, rank_points=0):
-    a = Answer.objects.create(question_title=question_title)
+def add_answer(user: User, question: Question, content, rank_count=0, rank_points=0):
+    # a = Answer.objects.create(question=question,
+    #                           user=user,
+    #                           content=content,
+    #
+    #                           )
     a.user = user
     a.question = question
     a.content = content
@@ -82,7 +90,7 @@ def add_answer(user: User, question: Question, question_title, content, rank_cou
 
 
 def add_profile(popular, user):
-    p = UserProfile.objects.get_or_create(popular=popular, user=user)
+    p = UserProfile.objects.get_or_create(popular=popular, user=user)[0]
     p.save()
     return p
 
