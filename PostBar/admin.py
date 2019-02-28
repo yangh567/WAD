8c23b3a6d2ca1e5fa6362b4ae6980dcc7c2e9d26
zhouyang shen <2359009s@student.gla.ac.uk>
2019-02-28 20:05:20 +0000

    update admin view

--- PostBar/admin.py
+++ PostBar/admin.py
@@ -1,7 +1,50 @@
-from django.contrib import admin
-from PostBar.models import Category, Question,Answer,UserProfile
+from pprint import pprint
 
-admin.site.register(Category)
-admin.site.register(Question)
+from django.contrib import admin
+from PostBar.models import Category, Question, Answer, UserProfile
+
+from django.contrib import admin
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        AnswerInline,
+    ]
+
+    def icount(self, obj):
+        print(dir(obj))
+        return obj.answers.count()
+
+    list_display = ["title", "icount"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionInline,
+    ]
+
+    def icount(self, obj):
+        return obj.questions.count()
+
+    list_display = ["name", "icount"]
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    def name(self, obj):
+        return obj.user.username
+
+    list_display = ["name"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
