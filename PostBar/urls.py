ca167385a31b13b7dac9809caa8e3c08fd1afac2
“Yixuan <2397832d@student.gla.ac.uk>
2019-03-04 17:58:21 +0000

    Merge branch 'master' of /Users/dyx/PycharmProjects/WAD with conflicts.

--- PostBar/urls.py
+++ PostBar/urls.py
@@ -3,6 +3,7 @@ from PostBar import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
     url(r'^about/',views.about,name='about'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
