50c62fc2a53e11995afa3708fbb4e8512e5b2b9c
zhouyang shen <2359009s@student.gla.ac.uk>
2019-03-06 09:49:45 +0000

    add fields to userprofile forms

--- PostBar/forms.py
+++ PostBar/forms.py
@@ -26,4 +26,5 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('website', 'picture')
+        fields = ('website', 'picture', 'location', 'background')
+
