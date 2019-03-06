67705e0d68cfe2f45f17896158d2baa4d1e7069d
zhouyang shen <2359009s@student.gla.ac.uk>
2019-03-06 16:17:37 +0000

    add additional function to models

--- PostBar/models.py
+++ PostBar/models.py
@@ -39,6 +39,21 @@ class UserProfile(models.Model):
     picture = models.ImageField(upload_to='profile_images', blank=True)
     location = models.CharField(max_length=128, default="")
     background = models.TextField(default="")
+    followings = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+
+    def add_following(self, user_id):
+        user = User.objects.get(user_id=user_id)
+        if user:
+            self.followings.add(user.userprofile)
+            return True
+        return False
+
+    def delete_following(self, user_id):
+        user = User.objects.get(user_id=user_id)
+        if user:
+            self.followings.remove(user.userprofile)
+            return True
+        return False
 
     def __str__(self):
         return self.user.username
