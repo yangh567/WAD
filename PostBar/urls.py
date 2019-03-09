59ddb6a0f6902a5cfd888be235dea9ee4f0e1161
“Yixuan <2397832d@student.gla.ac.uk>
2019-03-09 17:57:29 +0000

    additional changes on views

--- PostBar/urls.py
+++ PostBar/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
 
     # user profile
     url(r'^user_profile_detail/(?P<user_id>\d+)/$', views.user_profile_detail, name='user_profile_detail'),
-    url(r'^edit_user_profile/$', views.edit_user_profile, name='edit_user_profile'),
-    url(r'^user_profile_list/(?P<page>\d+)$', views.user_profile_list, name='user_profile_list'),
+    url(r'^edit_user_profile/$', views.user_profile_edit, name='edit_user_profile'),
+    url(r'^user_profile_list/(?P<page>\d+)$', views.user_profile_edit, name='user_profile_list'),
 
     # following system
     url(r'^following_list/(?P<user_id>\d+)/(?P<page>\d+)$', views.following_list, name='following_list'),
