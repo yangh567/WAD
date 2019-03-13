8040b3bd2966d884103290a95dfdcdaff39a9281
zhouyang shen <2359009s@student.gla.ac.uk>
2019-03-13 04:35:11 +0000

    add query function

--- static/js/base.js
+++ static/js/base.js
@@ -22,6 +22,36 @@ function update_ip_info() {
     });
 }
 
+function query() {
+    console.log("oke");
+    let search = $("#search");
+    let search_url = $("#search_url").val();
+
+    search.keyup(function (event) {
+        if (event.keyCode === 13) {
+            val = search.val();
+            if (val) {
+                console.log(val);
+                window.location.href = search_url + '?query=' + val;
+                localStorage["last_search"] = val
+            } else {
+                localStorage["last_search"] = ""
+            }
+        }
+    });
+    search.mouseleave(function (e) {
+        console.log("mouse_leave");
+        localStorage["last_search"] = $("#search").val()
+    })
+}
+
+function restore_query() {
+    let search = $("#search");
+    search.val(localStorage["last_search"])
+}
+
 $(function () {
     update_ip_info();
+    query();
+    restore_query();
 });
