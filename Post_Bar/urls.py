b2bddcfa0bc6ec67432081a3d0d3185a3c54aa1c
zhouyang shen <2359009s@student.gla.ac.uk>
2019-03-22 08:25:34 +0000

    preparing

--- Post_Bar/urls.py
+++ Post_Bar/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import password_reset_confirm, password_reset_complete
+
 from PostBar import views
 from django.conf import settings
 from django.conf.urls.static import static
